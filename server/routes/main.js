497b254d455c2809c40466ea0d5b9f29bee90e1b
phungNam <phungtuannam78@gmail.com>
2024-09-10 16:13:47 +0700

    layout with ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello world')
+  const locals = {
+    title: 'Nodejs Blog',
+    description: 'Simple Blog created with Nodejs',
+  }
+  res.render('index', { locals })
+})
+
+router.get('/about', (req, res) => {
+  res.render('about')
 })
 
 module.exports = router
